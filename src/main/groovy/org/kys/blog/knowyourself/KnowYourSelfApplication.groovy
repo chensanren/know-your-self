de02662892575efb7dc61f5525e53df74089d010
shuaichen <chenshuai123456you@163.com>
2021-07-26 11:36:25 +0800

    Init Project

---
+++ src/main/groovy/org/kys/blog/knowyourself/KnowYourSelfApplication.groovy
@@ -0,0 +1,13 @@
+package org.kys.blog.knowyourself
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class KnowYourSelfApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(KnowYourSelfApplication, args)
+    }
+
+}
---
+++ src/test/groovy/org/kys/blog/knowyourself/KnowYourSelfApplicationTests.groovy
@@ -0,0 +1,13 @@
+package org.kys.blog.knowyourself
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KnowYourSelfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
